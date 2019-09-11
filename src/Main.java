0a2a6aacd5c15de0c5a49d154ffe20293ba84756
Lacertilia <caramba97979@hotmail.com>
2019-09-11 14:25:34 +0200

    Added show messages and add message

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,41 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("1.Visa meddelanden \n2.Lägg till meddelande \n3.Uppdatera meddelande \n4.Spara meddelanden till fil \n5.Läs in meddelande från fil \n6.Avsluta ");
-        int in = scan.nextInt();
-        switch(in){
-            case 1:
+        while(true) {
+            menu();
+            int in = scan.nextInt();
+            ArrayList messages = new ArrayList();
+            switch (in) {
+                case 1:
+                    messages.forEach((n) -> System.out.println(n.toString()));
+                case 2:
+                    System.out.println("Skriv ditt meddelande.");
+                    messages.add(new Message(scan.next()));
+                case 3:
 
-            case 2:
+                case 4:
 
-            case 3:
+                case 5:
 
-            case 4:
+                case 6:
+                    System.out.println("See you next time.");
+                    System.exit(3);
 
-            case 5:
-
-            case 6:
-                System.exit(3);
-
+            }
         }
+    }
+
+    public static void menu(){
+        System.out.println("1.Visa meddelanden \n" +
+                "2.Lägg till meddelande \n" +
+                "3.Uppdatera meddelande \n" +
+                "4.Spara meddelanden till fil \n" +
+                "5.Läs in meddelande från fil \n" +
+                "6.Avsluta ");
     }
 }
