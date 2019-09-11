1ca71045d7876b74ff12cc34f72a622a8594538f
Lacertilia <caramba97979@hotmail.com>
2019-09-11 11:33:02 +0200

    Added Switch case for input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1.Visa meddelanden \n2.Lägg till meddelande \n3.Uppdatera meddelande \n4.Spara meddelanden till fil \n5.Läs in meddelande från fil \n6.Avsluta ");
+        int in = scan.nextInt();
+        switch(in){
+            case 1:
 
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+                System.exit(3);
+
+        }
     }
 }
