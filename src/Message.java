83ead3cfef35b0020dbaec1b5f66435dc20c2640
Lacertilia <caramba97979@hotmail.com>
2019-09-11 10:14:33 +0200

    Added Constructor for Message

--- src/Message.java
+++ src/Message.java
@@ -5,4 +5,15 @@ public class Message {
     private Date updatedAt;
     private String message;
     private String author;
+
+    public Message(String message){
+        if(message.length() <= 140){
+            this.message = message;
+        }else{
+            System.out.println("Your message is too long! Max. 140 characters");
+            System.exit(2);
+        }
+        this.createdAt = new Date();
+        this.author = System.getProperty("user.name");
+    }
 }
