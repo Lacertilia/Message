33c7d2024512776ac0f399baf3f26f9c0c100ff8
Lacertilia <caramba97979@hotmail.com>
2019-09-11 14:31:13 +0200

    Added update message

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int in;
+        ArrayList<Message> messages = new ArrayList<Message>();
         while(true) {
             menu();
-            int in = scan.nextInt();
-            ArrayList messages = new ArrayList();
+            in = scan.nextInt();
             switch (in) {
                 case 1:
                     messages.forEach((n) -> System.out.println(n.toString()));
+                    break;
                 case 2:
                     System.out.println("Skriv ditt meddelande.");
                     messages.add(new Message(scan.next()));
+                    break;
                 case 3:
-
+                    System.out.println("Vilket meddelande vill du uppdatera?");
+                    int update = scan.nextInt();
+                    System.out.println("Skriv ditt nya meddelande.");
+                    messages.get((update-1)).update(scan.next());
+                    System.out.println("Uppdateraderingen gick som den skulle");
                 case 4:
 
                 case 5:
@@ -25,7 +32,6 @@ public class Main {
                 case 6:
                     System.out.println("See you next time.");
                     System.exit(3);
-
             }
         }
     }
